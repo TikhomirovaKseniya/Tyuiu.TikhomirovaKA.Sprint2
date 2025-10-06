11c5b52cb83c8bb2a7c85205d49abbde317cd022
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-06 15:35:40 +0500

    Task2

---
+++ Tyuiu.TikhomirovaKA.Sprint2.Task2.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TikhomirovaKA.Sprint2.Task2.V4.Lib
+{
+    public class DataService : ISprint2Task2V4
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            return ((x >= 3 && x <= 5) && (y >= 3 && y <= 4)) || (x == 4 && y >= 8 && y <= 13) || ((x == 5 || x == 6) && y == 14) || ((x >= 5 && x <= 9) && (y >= 5 && y <= 7)) || (x == 9 && (y == 3 || y == 4)) || ((x == 3 || x == 4) && y == 7) || ((x >= 7 && x <= 10) && (y >= 8 && y <= 12)) || ((x == 11 || x == 12) && y == 11) || (x == 3 && y == 11) || ((x >= 11 && x <= 13) && (y >= 6 && y <= 8) && !(x == 11 && y == 6)) || (x == 12 && y >= 3 && y <= 5);
+        }
+    }
+}
---
+++ Tyuiu.TikhomirovaKA.Sprint2.Task2.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.TikhomirovaKA.Sprint2.Task2.V4.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила Тихомирова К. А. | ИБКСб-25-1";
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #2                                                              *");
+        Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                              *");
+        Console.WriteLine("* Задание #2                                                             *");
+        Console.WriteLine("* Вариант #4                                                             *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Введите точку X                                                        *");
+        int x;
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("* Введите точку Y                                                        *");
+        int y;
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+
+        if (ds.CheckDotInShadedArea(x, y) == true)
+        {
+            Console.WriteLine("Точка принадлежит области");
+        }
+        else 
+        {
+            Console.WriteLine("Точка не принадлежит области");
+        }
+        Console.ReadKey();
+    }
+}
